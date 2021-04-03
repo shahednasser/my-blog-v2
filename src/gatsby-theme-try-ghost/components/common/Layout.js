be903904f3904052fc69a4ebe4bc44e98cdbbed8
Shahed Nasser <shahednasser@gmail.com>
2021-04-04 00:26:16 +0300

    removed sharp

---
+++ src/gatsby-theme-try-ghost/components/common/Layout.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import Layout from 'gatsby-theme-try-ghost/components/common/Layout'
+
+function NewLayout (props) {
+    useEffect(() => {
+        const d = document,
+              s = d.createElement('script'),
+              previous = d.getElementById('carbonads');
+
+        if (previous) {
+            d.body.removeChild(previous)
+        }
+
+        s.src = 'https://cdn.carbonads.com/carbon.js?serve=CEBIL27J&placement=blogshahednassercom';
+        s.id = '_carbonads_js';
+        s.setAttribute('async', 'async');
+        d.body.appendChild(s);
+
+        return () => {
+            d.body.removeChild(s);
+        }
+    }, []);
+    
+    return <Layout props={props} />
+}
+
+export default NewLayout
