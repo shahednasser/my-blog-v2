676be2053de775e65afbe6883733ddf4832e9eb2
Shahed Nasser <shahednasser@gmail.com>
2021-04-23 22:43:28 +0000

    added search + monetization

--- src/gatsby-theme-try-ghost/components/common/Layout.js
+++ src/gatsby-theme-try-ghost/components/common/Layout.js
@@ -3,21 +3,42 @@ import Layout from 'gatsby-theme-try-ghost/src/components/common/Layout'
 
 function NewLayout (props) {
     useEffect(() => {
-        const d = document,
-              s = d.createElement('script'),
+        const d = document
+
+        function showAds () {
+            const s = d.createElement('script'),
               previous = d.getElementById('carbonads');
 
-        if (previous) {
-            d.body.removeChild(previous)
+            if (previous) {
+                d.body.removeChild(previous)
+            }
+
+            s.src = 'https://cdn.carbonads.com/carbon.js?serve=CEBIL27J&placement=blogshahednassercom';
+            s.id = '_carbonads_js';
+            s.setAttribute('async', 'async');
+            d.body.appendChild(s);
+        }
+        
+        function monetizationStart (event) {
+            if (!document.monetization.state === "started") {
+                showAds();
+            }
         }
 
-        s.src = 'https://cdn.carbonads.com/carbon.js?serve=CEBIL27J&placement=blogshahednassercom';
-        s.id = '_carbonads_js';
-        s.setAttribute('async', 'async');
-        d.body.appendChild(s);
+        if (document.monetization) {
+            document.monetization.addEventListener("monetizationstart", monetizationStart);
+        } else {
+            showAds();
+        }
 
         return () => {
-            d.body.removeChild(s);
+            if (d.getElementById('_carbonads_js')) {
+                d.body.removeChild('_carbonads_js')
+            }
+            if (d.getElementById('carbonads')) {
+                d.body.removeChild('carbonads')
+            }
+            document.monetization.removeEventListener("monetizationstart", monetizationStart);
         }
     }, []);
     
---
+++ src/gatsby-theme-try-ghost/components/common/Search.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { useFlexSearch } from 'react-use-flexsearch'
+import { useStaticQuery, graphql } from "gatsby"
+
+const Search = () => {
+    const queryData = useStaticQuery(graphql`
+        query  {
+            localSearchPages {
+            index
+            store
+            }
+        }
+    `)
+
+    const index = queryData.localSearchPages.index
+    const store = queryData.localSearchPages.store
+    const [query, setQuery] = useState('')
+    const [focused, setFocused] = useState(false)
+    const results = useFlexSearch(query, index, store)
+    const searchResultsRef = useRef(null)
+    const searchBarRef = useRef(null)
+
+    useEffect(() => {
+        if (results && results.length && focused) {
+            searchResultsRef.current.classList.add('show')
+        } else {
+            searchResultsRef.current.classList.remove('show')
+        }
+    }, [results, focused])
+
+    useEffect(() => {
+
+        function handleFocus () {
+            console.log(document.activeElement, document.activeElement.closest('.search-bar'), document.activeElement.closest('.search-results'),
+            searchBarRef.current, searchResultsRef.current)
+            setFocused(searchBarRef.current === document.activeElement ||
+                searchBarRef.current === document.activeElement.closest('.search-bar') ||
+                searchResultsRef.current === document.activeElement ||
+                searchResultsRef.current === document.activeElement.closest('.search-results'));
+        }
+
+        document.body.addEventListener('click', handleFocus)
+
+        return () => {
+            document.body.removeEventListener('click', handleFocus)
+        }
+    }, [])
+
+    return (
+        <div class="inner search-wrapper">
+        <input type="text" name="query" className="search-bar"
+            placeholder="Search..." onChange={(event) => setQuery(event.target.value)}
+            autocomplete="off" ref={searchBarRef} />
+        <div class="search-results" ref={searchResultsRef}>
+            <ul>
+                {results.map(result => (
+                    <li key={result.id}>
+                        <a href={result.path}>{result.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        </div>
+    )
+}
+
+export default Search
