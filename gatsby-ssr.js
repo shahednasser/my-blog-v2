fb5a68e6e769d1b7248c864a32430828c69e2819
Shahed Nasser <shahednasser@gmail.com>
2021-06-02 15:52:29 +0000

    fix for social tags

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,34 +1,16 @@
-const React = require("react")
-const { Helmet } = require("react-helmet")
-
-exports.onRenderBody = (
-  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
-  pluginOptions
-) => {
-  const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
-  setHeadComponents([
-    helmet.title.toComponent(),
-    helmet.link.toComponent(),
-    helmet.meta.toComponent(),
-    helmet.noscript.toComponent(),
-    helmet.script.toComponent(),
-    helmet.style.toComponent(),
-  ])
-}
+var React = require("react");
 
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  const headComponents = getHeadComponents()
+    /**
+     * @type {any[]} headComponents
+     */
+    const headComponents = getHeadComponents();
 
-  headComponents.sort((x, y) => {
-    if (x.props && x.props["data-react-helmet"]) {
-      return -1
-    } else if (y.props && y.props["data-react-helmet"]) {
-      return 1
-    }
-    return 0
-  })
-
-  replaceHeadComponents(headComponents)
-}
+    headComponents.sort((a, b) => {
+        if (a.props && a.props["data-react-helmet"]) {
+            return 0;
+        }
+        return 1;
+    });
+    replaceHeadComponents(headComponents);
+};
