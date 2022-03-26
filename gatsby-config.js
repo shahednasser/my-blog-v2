998990d182f331b6d747e34d9dea83b2d1924308
Shahed Nasser <shahednasser@gmail.com>
2022-03-26 17:24:28 +0200

    change to G4 analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,18 +98,20 @@ module.exports = {
             resolve: `gatsby-plugin-gatsby-cloud`,
         },
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-                // The property ID; the tracking code won't be generated without it
-                trackingId: `UA-149400351-5`,
-                // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: true,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Delays sending pageview hits on route update (in milliseconds)
-                pageTransitionDelay: 0,
-                // Defers execution of google analytics script after page load
-                defer: true,
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    `G-LLSH3924KR`, // Google Analytics / GA
+                    `UA-149400351-5`,
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                    head: true,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                },
             },
         },
         {
